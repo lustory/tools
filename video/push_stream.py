49ed3df89a88a21e3df71d2575a201ffa2dee28e
lulei <lustory@163.com>
2021-08-25 20:28:24 +0800

    update

---
+++ video/push_stream.py
@@ -0,0 +1,44 @@
+import subprocess
+
+class PUSH_STREAM:
+    def __init__(self, ip, port, stream_name, image_width, image_height, fps):
+        self.ip = ip
+        self.port = port
+        self.stream_name = stream_name
+        self.width = image_width
+        self.height = image_height
+        self.fps = fps 
+        self.push_process = None
+        
+    def start_prush_rtmp_stream(self):
+        '''
+            ip: 推流ip
+            port: 推流端口
+            stream_name: 推流名称
+            image_width: 视频宽度
+            image_height: 视频高度
+            fps: 视频帧率
+        '''
+        # rtmp 推流地址
+        rtmp_url = f"rtmp://{self.ip}:{self.port}/stream/{self.stream_name}"
+            
+        # ffmpeg命令
+        command = ['ffmpeg',
+                   '-y',
+                   '-f', 'rawvideo',
+                   '-vcodec', 'rawvideo',
+                   '-pix_fmt', 'bgr24',
+                   '-s', "{}x{}".format(self.width, self.height),
+                   '-r', str(self.fps),
+                   '-i', '-',
+                   '-c:v', 'libx264',
+                   '-pix_fmt', 'yuv420p',
+                   '-preset', 'ultrafast',
+                   '-f', 'flv',
+                   rtmp_url]
+        
+        # 开启子进程
+        self.push_process = subprocess.Popen(command, stdin=subprocess.PIPE)
+        
+    def image_write(self, image):
+        self.push_process.stdin.write(image.tobytes())
